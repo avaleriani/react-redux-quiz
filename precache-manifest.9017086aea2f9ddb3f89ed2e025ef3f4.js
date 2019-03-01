7759ffcb9d11370c892d40b75815c1ba64a1ab2f
Agu Valeriani <agvaleriani@gmail.com>
2019-03-01 01:24:45 +0100

    Updates

---
+++ precache-manifest.9017086aea2f9ddb3f89ed2e025ef3f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a3c1bb9f5a1a492957",
+    "url": "/react-redux-quiz/static/js/runtime~main.d2a3c1bb.js"
+  },
+  {
+    "revision": "1f490fa9747efc493eb0",
+    "url": "/react-redux-quiz/static/js/main.1f490fa9.chunk.js"
+  },
+  {
+    "revision": "1b23ea3883f43792fe82",
+    "url": "/react-redux-quiz/static/js/2.1b23ea38.chunk.js"
+  },
+  {
+    "revision": "1f490fa9747efc493eb0",
+    "url": "/react-redux-quiz/static/css/main.16fbd298.chunk.css"
+  },
+  {
+    "revision": "88650442b0a5a595d4453cca3f54a7df",
+    "url": "/react-redux-quiz/index.html"
+  }
+];
